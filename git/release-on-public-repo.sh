47f8c3ae0fec715e369f972658d073bbd392ff67
Jürij Pigowski <iurii.pigovskyi@fh-dortmund.de>
2025-02-21 14:14:52 +0100

    add script to publish a private repo

---
+++ git/release-on-public-repo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+. ~/release-config.sh
+
+cd "$PUB_REPO_DIR" || exit $?
+
+LAST_PUB_VERSION="$(git describe --abbrev=0 --tags)"
+
+cd "$PRI_REPO_DIR" || exit $?
+
+LAST_PRI_VERSION="$(git describe --abbrev=0 --tags)"
+
+git diff "$LAST_PUB_VERSION..$LAST_PRI_VERSION" > /tmp/"$LAST_PRI_VERSION".diff
+
+cd "$PUB_REPO_DIR" || exit $?
+
+git apply /tmp/"$LAST_PRI_VERSION".diff || exit $?
+
+git add . || exit $?
+
+git commit -m "add $LAST_PRI_VERSION" || exit $?
+
+git tag $LAST_PRI_VERSION || exit $?
+
+git push --tags origin HEAD || exit $?
+
