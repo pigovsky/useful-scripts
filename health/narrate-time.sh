dd52652cc7906c6052b5bea40033d02d143f7fc0
Yeva Pigovska <pigovsky+yeva@gmail.com>
2026-08-25 07:57:32 +0200

    narrate time

--- health/narrate-time.sh
+++ health/narrate-time.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Source - https://stackoverflow.com/a/3355423
+# Posted by ndim, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-08-25, License - CC BY-SA 4.0
+
+cd "$(dirname "$0")"
 
 spd-say -l de "Es ist jetzt $(date +'%H Uhr %M')"
 sleep 5
